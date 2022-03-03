7277e412189c92c92c19f5472c19a6255286b407
Michael Kong <michael@vivodigital.com.au>
2022-03-04 00:47:21 +1100

    update signup auth service to handle exception and add login auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDTO } from './dto';
@@ -7,28 +8,55 @@ import { AuthDTO } from './dto';
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  login() {
-    return "I've logged in";
-  }
-
-  async signUp(dto: AuthDTO) {
-    console.log('dto in auth service: ', dto);
-    const hash = await argon.hash(dto.password);
-
-    const user = await this.prisma.user.create({
-      data: {
+  async login(dto: AuthDTO) {
+    const user = await this.prisma.user.findUnique({
+      where: {
         email: dto.email,
-        hash,
-      },
-      select: {
-        id: true,
-        email: true,
-        createdAt: true,
-        firstName: true,
-        lastName: true,
       },
     });
 
+    if (!user) {
+      throw new ForbiddenException('Login credential is incorrect');
+    }
+
+    const isPasswordMatched = await argon.verify(user.hash, dto.password);
+
+    if (!isPasswordMatched) {
+      throw new ForbiddenException('Login credential is incorrect');
+    }
+
+    delete user.hash;
+
     return user;
+  }
+
+  async signUp(dto: AuthDTO) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+          firstName: true,
+          lastName: true,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        // "Unique constraint failed on the {constraint}"
+        throw new ForbiddenException('User already exists');
+      }
+
+      throw error;
+    }
   }
 }
