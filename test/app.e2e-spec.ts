d47495fa3bb000b163ea3b7b5e18b693bdf3c47d
Michael Kong <michael@vivodigital.com.au>
2022-03-16 00:10:55 +1100

    Add Auth sign up and login tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,17 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as pactum from 'pactum';
 import { Test } from '@nestjs/testing';
 import { PrismaService } from './../src/prisma/prisma.service';
 import { AppModule } from './../src/app.module';
+import { AuthDTO } from 'src/auth/dto';
 
 describe('Test e2e', () => {
   let app: INestApplication;
   let prisma: PrismaService;
+  const dto: AuthDTO = {
+    email: 'michael@kong.com',
+    password: '1234',
+  };
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -19,6 +25,9 @@ describe('Test e2e', () => {
       }),
     );
     await app.init();
+    await app.listen(9999);
+
+    pactum.request.setBaseUrl('http://localhost:9999');
     prisma = app.get(PrismaService);
     prisma.cleanDB();
   });
@@ -27,5 +36,86 @@ describe('Test e2e', () => {
     app.close();
   });
 
-  it.todo('Test auth');
+  describe('Auth', () => {
+    describe('Sign up', () => {
+      it('Should throw if no email provided', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+
+      it('Should throw if no password provided', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      });
+
+      it('Should throw if no body provided', () => {
+        return pactum.spec().post('/auth/signup').expectStatus(400);
+      });
+
+      it('Should sign up', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+
+    describe('Login', () => {
+      it('Should throw if no email provided', () => {
+        return pactum
+          .spec()
+          .post('/auth/login')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+
+      it('Should throw if no password provided', () => {
+        return pactum
+          .spec()
+          .post('/auth/login')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      });
+
+      it('Should throw if no body provided', () => {
+        return pactum.spec().post('/auth/login').expectStatus(400);
+      });
+
+      it('Should throw if wrong email and password combination provided', () => {
+        return pactum
+          .spec()
+          .post('/auth/login')
+          .withBody({
+            email: dto.email,
+            password: `${dto.password}wrong`,
+          })
+          .expectStatus(403);
+      });
+
+      it('Should login', () => {
+        return pactum
+          .spec()
+          .post('/auth/login')
+          .withBody(dto)
+          .expectStatus(200);
+      });
+    });
+  });
+  it.todo('User');
+  it.todo('Bookmark');
 });
